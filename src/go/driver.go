870a9f71ebe81bb46fc324ce41ee8bd295503cc2
Peter Park <peter@Peters-MacBook-Pro.local>
2018-03-11 20:53:01 -0400

    Add driver.go and driverconfig.json
    Currently driver.go is able to unmarshal driverconfig.json and retrieve
    all the objects (up to 1000) that match a certain prefix

---
+++ src/go/driver.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+type LambdaFunction struct {
+	Name    string `json:"name"`
+	Handler string `json:"handler"`
+	Zip     string `json:"handler"`
+}
+
+type configFile struct {
+	Bucket             string         `json:"bucket"`
+	Prefix             string         `json:"prefix"`
+	JobBucket          string         `json:"jobBucket"`
+	Region             string         `json:"region"`
+	LambdaMemory       int            `json:"lambdaMemory"`
+	ConcurrentLambdas  int            `json:"concurrentLambdas"`
+	Mapper             LambdaFunction `json:"mapper"`
+	Reducer            LambdaFunction `json:"reducer"`
+	ReducerCoordinator LambdaFunction `json:"reducerCoordinatory"`
+}
+
+func main() {
+	//  JOB ID
+	jobID := os.Args[1]
+	fmt.Println("Starting job %s", jobID)
+
+	// Retrieve the values in driverconfig.json
+	raw, err := ioutil.ReadFile("./driverconfig.json")
+	if err != nil {
+		panic(err)
+	}
+	var config configFile
+	err = json.Unmarshal(raw, &config)
+	if err != nil {
+		panic(err)
+	}
+
+	bucket := config.Bucket
+	jobBucket := config.JobBucket
+	region := config.Region
+	lambdaMemory := config.LambdaMemory
+	concurrentLambdas := config.ConcurrentLambdas
+
+	// Fetch the keys that match the prefix from the config
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1"),
+	}))
+	s3Client := s3.New(sess)
+	listObjectsInput := new(s3.ListObjectsInput)
+	listObjectsInput = listObjectsInput.SetBucket(bucket)
+	listObjectsInput = listObjectsInput.SetPrefix(config.Prefix)
+	var maxKeys int64 = 1000
+	listObjectsInput = listObjectsInput.SetMaxKeys(maxKeys)
+	listObjectsOutput, err := s3Client.ListObjects(listObjectsInput)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(listObjectsOutput)
+}
